3d21ca9ee777a6ba5ffe9e28bc0a68cf6f01005c
Nolan <nolannaphys@gmail.com>
2023-04-26 13:14:59 -0400

    Working code into Bootstrap template

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,30 +7,17 @@ $("#searchButton").on("click",function(){
 })
 
 
-
-function geoCode(searchValue){
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${searchValue}&limit=1&appid=${apiKey}`)
-    .then(response => response.json())
-    .then(data =>{
-        console.log (data)
-        currentWeather(data[0].lat,data[0].lon)
-        forecast(data[0].lat,data[0].lon)
-    })
-}
-
-
-
 var currentDate= $("<h2>").text(dayjs().format("MM/DD/YYYY"))
 function currentWeather(lat,lon){
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`)
     .then(response => response.json())
     .then(data =>{
         console.log (data)
-var cityName=$("<h2>").text(data.name)
+$("#cityInput").text(data.name)
 var temp=$("<h3>").text("Temp: "+data.main.temp)
-var wind=$("<h3>").text("Wind Speed: "+data.wind.speed)
-var humidity=$("<h3>").text("Humidity: "+data.main.humidity)
-var icon=$("<img>").attr("src",`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`)
+// var wind=$("<h3>").text("Wind Speed: "+data.wind.speed)
+// var humidity=$("<h3>").text("Humidity: "+data.main.humidity)
+// var icon=$("<img>").attr("src",`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`)
 
 
         $("#currentWeather").append(currentDate,cityName,temp,wind,humidity,icon)
@@ -40,9 +27,20 @@ var icon=$("<img>").attr("src",`https://openweathermap.org/img/wn/${data.weather
 
 
 function forecast(lat,lon){
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`)
     .then(response => response.json())
     .then(data =>{
         console.log (data)
+    // data.list[0].main.temp
+    })
+}
+
+function geoCode(searchValue){
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${searchValue}&limit=1&appid=${apiKey}`)
+    .then(response => response.json())
+    .then(data =>{
+        console.log (data)
+        currentWeather(data[0].lat,data[0].lon)
+        forecast(data[0].lat,data[0].lon)
     })
 }
